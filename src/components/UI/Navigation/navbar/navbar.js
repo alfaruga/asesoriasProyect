09f8220f8e56bbb371174120057428172fc36185
Alexis Ruiz <alfaruga@gmail.com>
2020-09-29 19:22:14 -0500

    Some basic styling in headers

--- src/components/UI/Navigation/navbar/navbar.js
+++ src/components/UI/Navigation/navbar/navbar.js
@@ -7,16 +7,16 @@ import classes from "./navbar.module.scss";
 const navbar = (props) => {
 
     return (
-        <header>
-            <FontAwesomeIcon className={classes.Icon} onClick={props.clicked} icon={["fa", "bars"]} /><img
+        <header className={classes.Logos}>
+            <FontAwesomeIcon className={classes.Icon} onClick={props.clicked} icon={["fa", "bars"]} />
+            <div ><img
                 alt="logo"
                 src="https://firebasestorage.googleapis.com/v0/b/alexis-ruiz-asesorias.appspot.com/o/R.jpg?alt=media&token=2f4c2ff6-cd6d-48da-bfb0-e885971ce188"
-                width="50px"
-                height="50px"
-                className="d-inline-block align-top"
-            />Asesorías
-    R<sup>3</sup>
-
+                width="100px"
+                height="100px"
+            /><p>Asesorías
+    R<sup>3</sup></p>
+            </div>
             <ul className={classes.NavList}>
                 {props.sections.map(section => {
                     return (<NavItem name={section} section={section} key={section} ></NavItem>)
--- src/containers/WelcomeSection/WelcomeSection.js
+++ src/containers/WelcomeSection/WelcomeSection.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Section from "../../components/UI/Section/Section";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classes from "./WelcomeSection.module.scss"
 
 const welcomeSection = (props) => {
     return (
         <Section id={"#welcome"} href={"Welcome"}>
-            <div>
+            <div className={classes.WelcomeSection}>
                 <h1>Asesorías personalizadas</h1>
                 <h3>Te brindamos apoyo para cualquier tipo de exámenes.</h3>
                 <FontAwesomeIcon icon={["fab", "whatsapp-square"]} />8120269293
