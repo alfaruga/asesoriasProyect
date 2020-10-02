445bcbc4fab0bcfd97cdb8227d09e7630ba655d5
Alexis Ruiz <alfaruga@gmail.com>
2020-10-02 06:39:35 -0500

    Updates

---
+++ precache-manifest.9e2f1d8d8df274717cb85633059eaae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cad5bf187c91fdb5db7ff718c7d92d3",
+    "url": "/asesoriasProyect/index.html"
+  },
+  {
+    "revision": "9c2a74adc27fd295cd10",
+    "url": "/asesoriasProyect/static/css/main.c0bce3b5.chunk.css"
+  },
+  {
+    "revision": "d9d0776d5776d26a2ba1",
+    "url": "/asesoriasProyect/static/js/2.3af1ee31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asesoriasProyect/static/js/2.3af1ee31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2a74adc27fd295cd10",
+    "url": "/asesoriasProyect/static/js/main.24bcbfb1.chunk.js"
+  },
+  {
+    "revision": "3057114d1da76c4723b9",
+    "url": "/asesoriasProyect/static/js/runtime-main.ca9654e6.js"
+  }
+]);
